9f9207721d047c736d30bca553c5539c377a6378
MD ARMAN HOSSEN <ahossain3324@gmail.com>
2024-07-07 02:03:53 +0600

    Describe your changes

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,6 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
-    cout << "hello world";
+{   int n,target;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >>target;
+
+    int sum,l = 0,m = 0,flag = 0;
+    for(int i = 0; i < 10; i++)
+    {   sum = 0;
+        int k = i;
+        for(int j = 0; j < 10; j++)
+        {   if(j == k)
+        {
+            continue;
+        }
+        else
+        {
+           sum = arr[i] + arr[j];
+        if(sum == target)
+        {
+            l = i;
+            m = j;
+            flag = 1;
+        } 
+        }
+        }
+        if(flag == 1)
+        {
+            break;
+        }
+    }
+    cout << l <<" "<< m;
+    return 0;
 }
